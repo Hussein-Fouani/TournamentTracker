4404657e674994df41b9c576b0d967cc96be2d1e
ABOALI112 <social.101.acc@gmail.com>
2021-09-28 14:58:07 +0300

    Add project files.

---
+++ TrackerLibrary/MatchUpEntryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchUpEntryModel
+    {
+        /// <summary>
+        /// Represent One Team in the Matchup
+        /// </summary>
+        public TeamModel TeamComputing { get; set; }
+
+        public double  Score { get; set; }
+        public MatchUpModel ParentMatchUp { get; set; }
+
+    }
+}
---
+++ TrackerLibrary/MatchUpModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchUpModel
+    {
+        public List<MatchUpEntryModel> Entries { get; set; } = new List<MatchUpEntryModel>();
+
+        public TeamModel Winnder { get; set; }
+
+        public int MatchUpRound { get; set; }
+    }
+}
