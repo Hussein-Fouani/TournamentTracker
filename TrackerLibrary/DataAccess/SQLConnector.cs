76828d29afc64101290166a7dd09ab3aea4c678c
ABOALI112 <social.101.acc@gmail.com>
2021-10-04 20:01:04 +0300

    creating a DataAccess Folder that contains the data related classes,and reestablish the related connections

--- TrackerLibrary/DataAccess/SQLConnector.cs
+++ TrackerLibrary/DataAccess/SQLConnector.cs
@@ -5,8 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SqlClient;
+using TrackerLibrary.Models;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.DataAccess
 {
     public class SQLConnector : IDataConnection
     {
